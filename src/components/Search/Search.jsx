c177efee0803d47a3e4842c9cd55f97d03162a26
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-17 17:46:55 +0100

    finished styling search term items

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { CgClose } from "react-icons/cg";
 import { ImSearch } from "react-icons/im";
+import { IoIosArrowForward } from "react-icons/io";
+
 import "./Search.css";
 const Search = ({ showSearchArea, setShowSearchArea, city }) => {
   const [value, setValue] = useState("");
   // Should store search history in local storage.
+  // Should be validating input
 
   // Default search history
   const [searchHistory, setSearchHistory] = useState([
@@ -23,6 +26,8 @@ const Search = ({ showSearchArea, setShowSearchArea, city }) => {
 
     setSearchHistory((prevSearchHistory) => [
       ...prevSearchHistory,
+
+      // Generating ids manually for the list-keys
       { id: prevSearchHistory[prevSearchHistory.length - 1].id + 1, searchTerm: value },
     ]);
 
@@ -52,7 +57,7 @@ const Search = ({ showSearchArea, setShowSearchArea, city }) => {
           return (
             <div key={history.id}>
               <p>{history.searchTerm}</p>
-              <p>Arrow</p>
+              <IoIosArrowForward className="arrow" />
             </div>
           );
         })}
