2087a933717b36cab7058f7982b2f6a1ef3333e1
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-17 09:31:36 +0100

    api now works , onto implemeting data

--- src/App.js
+++ src/App.js
@@ -6,29 +6,42 @@ import Main from "./components/Main/Main";
 
 import "./App.css";
 const App = () => {
+  const [todayData, setTodayData] = useState(null);
+
   function getLocation() {
-    const successCallback = (position) => {
+    const success = (position) => {
       const { latitude, longitude } = position.coords;
-      console.log(latitude, longitude);
+      // console.log(latitude, longitude);
       getWeather(latitude, longitude);
     };
 
-    const errorCallback = (error) => {
+    const error = (error) => {
       console.log(error);
-      // alert(error);
     };
 
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    navigator.geolocation.getCurrentPosition(success, error);
   }
-  function getWeather(latitude, longitude) {
-    try {
-    } catch (err) {
-      console.log(err);
-    }
-  }
+
   useEffect(() => {
     getLocation();
   }, []);
+
+  function getWeather(lat, lon) {
+    // console.log(lat, lon);
+    const API_key = "e416be1f249467ff1237c0b4e24aedd7";
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
+
+    axios
+      .get(url)
+      .then((res) => {
+        const data = res.data.list;
+        setTodayData(data[0]);
+        console.log(todayData);
+      })
+      .catch((err) => {
+        console.log(`Error ${err}`);
+      });
+  }
   return (
     <div className="App">
       <Aside />
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Forecast from "../Forecast/Forecast";
 import Units from "../Units/Units";
 import WindStatus from "../WindStatus/WindStatus";
@@ -8,29 +8,6 @@ import AirPressure from "../AirPressure/AirPressure";
 
 import "./Main.css";
 const Main = () => {
-  function getLocation() {
-    const successCallback = (position) => {
-      const { latitude, longitude } = position.coords;
-      console.log(latitude, longitude);
-      getWeather(latitude, longitude);
-    };
-
-    const errorCallback = (error) => {
-      console.log(error);
-      // alert(error);
-    };
-
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-  }
-  function getWeather(latitude, longitude) {
-    try {
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  useEffect(() => {
-    getLocation();
-  }, []);
   return (
     <div className="main">
       <section className="units-section">
