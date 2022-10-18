a982eb0d80676cb94542d78f833e35a5c8dbeba6
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-18 16:47:51 +0100

    all todays highlights now get dynamic data

--- src/components/Visibility/Visibility.jsx
+++ src/components/Visibility/Visibility.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 import "./Visibility.css";
 const Visibility = ({ visibility }) => {
+  const visibilityInMiles = (visibility / 1609).toFixed(1);
   return (
     <div className="visibility">
       <p>Visibility</p>
       <p>
-        <span className="visibility-value">6.4</span>
+        <span className="visibility-value"> {visibilityInMiles} </span>
         <span>miles</span>
       </p>
     </div>
